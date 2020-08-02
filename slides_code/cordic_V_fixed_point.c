6c607c3133e53540492de14360257a671063c1a2
Noah <noahwclarke@gmail.com>
2020-08-02 16:19:21 -0700

    Reduced branching hopefully

--- slides_code/cordic_V_fixed_point.c
+++ slides_code/cordic_V_fixed_point.c
@@ -8,31 +8,31 @@ int cordic_V_fixed_point(int xy, int *z) {
   int x_temp_2, y_temp_2;
   int i;
   
-  // clock_t start = clock();
-  // for(int j = 0; j < 100000; j++){
+  clock_t start = clock();
+  for(int j = 0; j < 100000; j++){
 
     x_temp_1 = xy & 0xffff;
     y_temp_1 = (xy >> 16) & 0xffff;
     z_temp = 0;
 
+    int sign;
+
     for( i=0; i<15; i++) { /* 15 iterations are needed */
-      if( y_temp_1 > 0) {
-        x_temp_2 = x_temp_1 + (y_temp_1 >> i);
-        y_temp_2 = y_temp_1 - (x_temp_1 >> i);
-        z_temp += z_table[i];
-      } else {
-        x_temp_2 = x_temp_1 - (y_temp_1 >> i);
-        y_temp_2 = y_temp_1 + (x_temp_1 >> i);
-        z_temp -= z_table[i];
-      }
+      
+      sign = y_temp_1 > 0 ? 1 : -1;
+      
+      x_temp_2 = x_temp_1 + (sign*(y_temp_1 >> i));
+      y_temp_2 = y_temp_1 - (sign*(x_temp_1 >> i));
+      z_temp += sign*z_table[i];
+
       x_temp_1 = x_temp_2;
       y_temp_1 = y_temp_2;
     }
 
-  // }
+  }
 
-  // // clock_t finish = clock();
-  // // printf("Execution time for 100000 iterations: %ld \n", finish - start);
+  clock_t finish = clock();
+  printf("Execution time for 100000 iterations: %ld \n", finish - start);
 
   *z = z_temp;
 
