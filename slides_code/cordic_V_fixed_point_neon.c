f644ad7b23cfa63af9559082edc5bc8672130183
Noah Clarke <noahwclarke@uvic.ca>
2020-08-03 23:39:11 -0700

    Wrong but working neon code

--- slides_code/cordic_V_fixed_point_neon.c
+++ slides_code/cordic_V_fixed_point_neon.c
@@ -14,8 +14,8 @@ int cordic_V_fixed_point(int xy, int *z) {
   xy_load[0] = xy & 0xffff;
   xy_load[1] = (xy >> 16) & 0xffff;
 
-  int32x2_t xy = vld1_s32(xy_load);
-  int32x2_t yx = vrev64_s32(xy);
+  int32x2_t xy_neon = vld1_s32(xy_load);
+  int32x2_t yx_neon = vrev64_s32(xy_neon);
 
   z_temp = 0;
 
@@ -25,17 +25,19 @@ int cordic_V_fixed_point(int xy, int *z) {
 
   for( i=0; i<15; i++) { /* 15 iterations are needed */
       
-    sign = xy[1] > 0 ? sign_A : sign_B;
+    sign = xy_neon[1] > 0 ? sign_A : sign_B;
 
-    xy = vshr_n_s32(xy, i); // shift
+    //xy_neon = vshr_n_s32(xy_neon, 4); // shift
+    xy_neon[0] = xy_neon[0] >> i;
+    xy_neon[1] = xy_neon[1] >> i;
 
-    xy = vmul_n_s32(xy, sign); // fix signs
+    xy_neon = vmul_s32(xy_neon, sign); // fix signs
 
-    xy = vadd_s32(xy, yx); // add
+    xy_neon = vadd_s32(xy_neon, yx_neon); // add
 
-    yx = xy; // save reversed
+    yx_neon = xy_neon; // save reversed
 
-    xy = vrev64_s32(xy); // fix orientation
+    xy_neon = vrev64_s32(xy_neon); // fix orientation
 
     z_temp += sign[0]*z_table[i]; // compute z
 
@@ -46,5 +48,5 @@ int cordic_V_fixed_point(int xy, int *z) {
 
   *z = z_temp;
 
-  return xy[1] << 16 | xy[0];
+  return xy_neon[1] << 16 | xy_neon[0];
 }
