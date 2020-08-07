264739d8378b2312ba647c4d64f2a88c07554de2
Noah <noahwclarke@gmail.com>
2020-08-07 15:51:35 -0700

    Updated neon

--- slides_code/cordic_V_fixed_point_neon.c
+++ slides_code/cordic_V_fixed_point_neon.c
@@ -5,7 +5,6 @@
 int z_table[15];
 
 int cordic_V_fixed_point(int xy, int *z) {
-  int xy_load[2];
   int z_temp;
   int i;
   
@@ -17,12 +16,10 @@ int cordic_V_fixed_point(int xy, int *z) {
   z_temp = 0;
 
   int32x2_t sign;
-  int32x2_t sign_A = {1, -1};
-  int32x2_t sign_B = {-1, 1};
 
   for( i=0; i<15; i++) { /* 15 iterations are needed */
       
-    sign = xy_neon[1] > 0 ? sign_A : sign_B;
+    int32x2_t sign = xy_neon[1] > 0 ? {1,-1} : {-1,1};
 
     // shift
     yx_neon[0] = yx_neon[0] >> i;
