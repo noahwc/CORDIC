1d0812bd169518f956407725f44a3a12d95efa1a
Noah Clarke <noahwclarke@uvic.ca>
2020-08-07 19:02:25 -0700

    updated assembly

--- slides_code/cordic_V_fixed_point_neon.s
+++ slides_code/cordic_V_fixed_point_neon.s
@@ -21,67 +21,53 @@
 cordic_V_fixed_point:
 	@ args = 0, pretend = 0, frame = 8
 	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, lr}
-	vpush.64	{d8}
-	mov	r5, r0
-	sub	sp, sp, #8
-	mov	r4, r1
-	bl	clock
-	lsr	r3, r5, #16
-	uxth	r5, r5
-	stm	sp, {r3, r5}
-	mov	r2, #0
+	push	{r4, r5, lr}
+	lsr	r3, r0, #16
+	sub	sp, sp, #12
+	uxth	r0, r0
+	str	r3, [sp]
+	str	r0, [sp, #4]
 	vldr	d16, [sp]
-	mov	r6, r0
-	vrev64.32	d8, d16
-	mov	r5, r2
-	ldr	lr, .L8
+	mov	r2, #0
+	vrev64.32	d17, d16
+	mov	r4, r2
+	ldr	r5, .L8
 .L3:
 	vmov.32	r3, d16[0]
-	vmov.32	r1, d8[1]
+	vmov.32	ip, d17[1]
 	asr	r3, r3, r2
 	vmov.32	r0, d16[1]
 	vmov.32	d16[0], r3
-	cmp	r1, #0
-	movgt	r1, #1
-	mvnle	r1, #0
+	cmp	ip, #0
+	movgt	lr, #1
+	mvnle	lr, #0
 	asr	r0, r0, r2
 	vmov.32	d16[1], r0
-	mul	r3, r1, r3
+	mul	r3, lr, r3
 	mvngt	ip, #0
 	movle	ip, #1
 	vmov.32	d16[0], r3
 	mul	r0, ip, r0
 	vmov.32	d16[1], r0
-	vadd.i32	d8, d8, d16
+	vadd.i32	d17, d17, d16
 	add	r2, r2, #1
-	ldr	r3, [lr, #4]!
+	ldr	r3, [r5, #4]!
 	cmp	r2, #15
-	mla	r5, r3, r1, r5
-	vrev64.32	d16, d8
+	mla	r4, r3, lr, r4
+	vrev64.32	d16, d17
 	bne	.L3
-	bl	clock
-	sub	r1, r0, r6
-	movw	r0, #:lower16:.LC0
-	movt	r0, #:upper16:.LC0
-	bl	printf
-	vmov.32	r0, d8[1]
-	vmov.32	r3, d8[0]
+	vmov.32	r0, d17[1]
+	vmov.32	r3, d17[0]
 	orr	r0, r3, r0, lsl #16
-	str	r5, [r4]
-	add	sp, sp, #8
+	str	r4, [r1]
+	add	sp, sp, #12
 	@ sp needed
-	vldm	sp!, {d8}
-	pop	{r4, r5, r6, pc}
+	pop	{r4, r5, pc}
 .L9:
 	.align	2
 .L8:
 	.word	z_table-4
 	.size	cordic_V_fixed_point, .-cordic_V_fixed_point
 	.comm	z_table,60,4
-	.section	.rodata.str1.4,"aMS",%progbits,1
-	.align	2
-.LC0:
-	.ascii	"Execution time: %ld \012\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
--- slides_code/cordic_V_fixed_point_nobranch.s
+++ slides_code/cordic_V_fixed_point_nobranch.s
@@ -16,182 +16,171 @@
 	.arch armv7-a
 	.syntax unified
 	.arm
-	.fpu neon
+	.fpu vfpv3-d16
 	.type	cordic_V_fixed_point, %function
 cordic_V_fixed_point:
-	@ args = 0, pretend = 0, frame = 32
+	@ args = 0, pretend = 0, frame = 24
 	@ frame_needed = 0, uses_anonymous_args = 0
+	lsrs	r3, r0, #16
 	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
-	sub	sp, sp, #36
-	mov	r4, r0
-	str	r1, [sp, #28]
-	bl	clock
-	str	r0, [sp, #4]
-	lsrs	r0, r4, #16
 	movne	r9, #1
 	mvneq	r9, #0
-	uxth	r4, r4
-	mls	r2, r9, r4, r0
+	uxth	r0, r0
+	mls	r2, r9, r0, r3
 	cmp	r2, #0
 	movgt	fp, #1
 	mvnle	fp, #0
-	mla	r4, r9, r0, r4
-	asr	r3, r4, #1
+	mla	r0, r9, r3, r0
+	asr	r3, r0, #1
 	mls	r3, fp, r3, r2
 	cmp	r3, #0
 	movgt	r8, #1
 	mvnle	r8, #0
 	asr	r2, r2, #1
-	mla	r0, fp, r2, r4
+	mla	r0, fp, r2, r0
 	asr	r2, r0, #2
 	mls	r2, r8, r2, r3
 	cmp	r2, #0
 	movgt	r7, #1
 	mvnle	r7, #0
-	asr	r1, r3, #2
-	mla	r1, r8, r1, r0
-	asr	r3, r1, #3
+	asr	r3, r3, #2
+	mla	ip, r8, r3, r0
+	asr	r3, ip, #3
 	mls	r3, r7, r3, r2
 	cmp	r3, #0
 	movgt	r6, #1
 	mvnle	r6, #0
-	asr	r2, r2, #3
-	mla	r1, r7, r2, r1
-	asr	r2, r1, #4
+	asr	r0, r2, #3
+	mla	r0, r7, r0, ip
+	asr	r2, r0, #4
 	mls	r2, r6, r2, r3
 	cmp	r2, #0
 	movgt	ip, #1
 	mvnle	ip, #0
 	asr	r3, r3, #4
-	mla	r1, r6, r3, r1
-	asr	r3, r1, #5
+	mla	r0, r6, r3, r0
+	asr	r3, r0, #5
 	mls	r3, ip, r3, r2
 	cmp	r3, #0
 	movgt	lr, #1
 	mvnle	lr, #0
 	asr	r2, r2, #5
-	mla	r1, ip, r2, r1
-	str	ip, [sp, #8]
-	asr	ip, r1, #6
-	mls	ip, lr, ip, r3
-	mov	r2, lr
-	cmp	ip, #0
-	movgt	lr, #1
-	mvnle	lr, #0
-	asr	r3, r3, #6
-	mla	r3, r2, r3, r1
-	asr	r1, r3, #7
-	mls	r1, lr, r1, ip
-	cmp	r1, #0
-	movgt	r4, #1
-	mvnle	r4, #0
-	str	r2, [sp, #12]
-	asr	r2, ip, #7
-	mla	r2, lr, r2, r3
-	asr	ip, r2, #8
-	mls	ip, r4, ip, r1
-	cmp	ip, #0
-	movgt	r5, #1
-	mvnle	r5, #0
-	asr	r3, r1, #8
-	mla	r0, r4, r3, r2
-	asr	r1, r0, #9
-	mls	r1, r5, r1, ip
-	mov	r10, r5
-	cmp	r1, #0
-	movgt	r5, #1
-	mvnle	r5, #0
-	asr	r3, ip, #9
-	mla	r3, r10, r3, r0
-	asr	ip, r3, #10
-	mls	ip, r5, ip, r1
-	cmp	ip, #0
-	str	r4, [sp, #20]
-	movgt	r4, #1
-	mvnle	r4, #0
-	asr	r2, r1, #10
-	mla	r2, r5, r2, r3
-	asr	r1, r2, #11
-	mls	r1, r4, r1, ip
-	cmp	r1, #0
-	str	lr, [sp, #16]
-	movgt	lr, #1
-	mvnle	lr, #0
-	movw	r3, #:lower16:z_table
-	asr	r0, ip, #11
-	mla	r0, r4, r0, r2
-	asr	r2, r0, #12
-	mls	r2, lr, r2, r1
+	mla	r0, ip, r2, r0
+	asr	r2, r0, #6
+	mls	r2, lr, r2, r3
 	cmp	r2, #0
-	movgt	ip, #1
-	mvnle	ip, #0
+	movgt	r5, #1
+	mvnle	r5, #0
+	asr	r3, r3, #6
+	mla	r0, lr, r3, r0
+	asr	r4, r0, #7
+	mls	r4, r5, r4, r2
+	mov	r3, r5
+	cmp	r4, #0
+	movgt	r5, #1
+	mvnle	r5, #0
+	asr	r2, r2, #7
+	mla	r0, r3, r2, r0
+	sub	sp, sp, #28
+	str	lr, [sp, #4]
+	asr	lr, r0, #8
+	mls	lr, r5, lr, r4
+	mov	r2, r5
+	cmp	lr, #0
+	movgt	r5, #1
+	mvnle	r5, #0
+	str	r3, [sp, #8]
+	asr	r3, r4, #8
+	str	ip, [sp]
+	mla	ip, r2, r3, r0
+	asr	r0, ip, #9
+	mov	r4, r5
+	mls	r0, r5, r0, lr
+	cmp	r0, #0
+	movgt	r5, #1
+	mvnle	r5, #0
+	asr	r3, lr, #9
+	mla	r3, r4, r3, ip
+	asr	lr, r3, #10
+	mls	lr, r5, lr, r0
+	mov	r10, r5
+	cmp	lr, #0
+	movgt	r5, #1
+	mvnle	r5, #0
+	str	r2, [sp, #12]
+	asr	r2, r0, #10
+	mla	r2, r10, r2, r3
+	asr	r0, r2, #11
+	mls	r0, r5, r0, lr
+	cmp	r0, #0
+	str	r4, [sp, #16]
+	movgt	r4, #1
+	mvnle	r4, #0
+	movw	r3, #:lower16:z_table
+	asr	ip, lr, #11
+	mla	ip, r5, ip, r2
+	asr	r2, ip, #12
+	mls	r2, r4, r2, r0
+	cmp	r2, #0
+	movgt	lr, #1
+	mvnle	lr, #0
 	movt	r3, #:upper16:z_table
-	str	r10, [sp, #24]
+	str	r10, [sp, #20]
 	ldr	r10, [r3, #4]
-	asr	r1, r1, #12
+	asr	r0, r0, #12
 	mul	r10, r10, fp
 	ldr	fp, [r3]
-	mla	r1, lr, r1, r0
+	mla	r0, r4, r0, ip
 	mla	r10, fp, r9, r10
 	ldr	r9, [r3, #8]
-	ldr	r0, [r3, #12]
+	ldr	ip, [r3, #12]
 	mla	r10, r9, r8, r10
-	mla	r10, r0, r7, r10
-	ldr	r0, [r3, #16]
-	ldr	r7, [sp, #12]
-	mla	r10, r0, r6, r10
-	ldr	r6, [sp, #8]
-	ldr	r0, [r3, #20]
+	mla	r10, ip, r7, r10
+	ldr	ip, [r3, #16]
+	ldr	r7, [sp, #8]
+	mla	r10, ip, r6, r10
+	ldr	ip, [r3, #20]
+	ldr	r6, [sp]
 	ldr	r8, [sp, #16]
-	mla	r0, r0, r6, r10
-	ldr	r6, [r3, #24]
-	ldr	r10, [sp, #24]
-	mla	r0, r6, r7, r0
-	asr	r7, r1, #13
-	mls	r7, ip, r7, r2
-	ldr	r6, [r3, #28]
-	cmp	r7, #0
-	mla	r0, r6, r8, r0
-	movgt	r8, #1
-	mvnle	r8, #0
+	mla	ip, ip, r6, r10
+	ldr	r10, [r3, #24]
+	ldr	r6, [sp, #4]
+	mla	r10, r10, r6, ip
+	asr	r6, r0, #13
+	mls	r6, lr, r6, r2
+	ldr	ip, [r3, #28]
+	cmp	r6, #0
+	mla	ip, ip, r7, r10
+	movgt	r7, #1
+	mvnle	r7, #0
 	asr	r2, r2, #13
-	mla	r1, ip, r2, r1
-	ldr	r6, [r3, #32]
-	ldr	r2, [sp, #20]
-	mla	r6, r6, r2, r0
+	ldr	r10, [r3, #32]
+	mla	r2, lr, r2, r0
+	ldr	r0, [sp, #12]
+	mla	ip, r10, r0, ip
 	ldr	r0, [r3, #36]
-	ldr	r2, [r3, #40]
-	mla	r6, r0, r10, r6
-	mla	r5, r2, r5, r6
-	ldr	r2, [r3, #44]
-	mla	r4, r2, r4, r5
-	ldr	r2, [r3, #48]
-	ldr	r5, [r3, #56]
-	mla	lr, r2, lr, r4
-	ldr	r2, [r3, #52]
-	asr	r4, r7, #14
-	mla	ip, r2, ip, lr
-	asr	r3, r1, #14
-	mla	r5, r5, r8, ip
-	mla	r4, r8, r4, r1
-	mls	r7, r8, r3, r7
-	bl	clock
-	ldr	r3, [sp, #4]
-	sub	r1, r0, r3
-	movw	r0, #:lower16:.LC0
-	movt	r0, #:upper16:.LC0
-	bl	printf
-	ldr	r3, [sp, #28]
-	orr	r0, r4, r7, lsl #16
-	str	r5, [r3]
-	add	sp, sp, #36
+	ldr	r10, [sp, #20]
+	mla	r0, r0, r8, ip
+	ldr	ip, [r3, #40]
+	mla	r0, ip, r10, r0
+	ldr	ip, [r3, #44]
+	mla	r5, ip, r5, r0
+	ldr	r0, [r3, #48]
+	ldr	ip, [r3, #56]
+	mla	r4, r0, r4, r5
+	ldr	r0, [r3, #52]
+	asr	r3, r6, #14
+	mla	lr, r0, lr, r4
+	asr	r0, r2, #14
+	mls	r0, r7, r0, r6
+	mla	r2, r7, r3, r2
+	mla	lr, ip, r7, lr
+	orr	r0, r2, r0, lsl #16
+	str	lr, [r1]
+	add	sp, sp, #28
 	@ sp needed
 	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
 	.size	cordic_V_fixed_point, .-cordic_V_fixed_point
 	.comm	z_table,60,4
-	.section	.rodata.str1.4,"aMS",%progbits,1
-	.align	2
-.LC0:
-	.ascii	"Execution time for 100000 iterations: %ld \012\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
