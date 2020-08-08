3928294f96b4ddb242d9de7233069bd981752e07
Noah Clarke <noahwclarke@uvic.ca>
2020-08-07 18:15:49 -0700

    neon fixed

--- slides_code/cordic_V_fixed_point_neon.c
+++ slides_code/cordic_V_fixed_point_neon.c
@@ -5,35 +5,32 @@
 int z_table[15];
 
 int cordic_V_fixed_point(int xy, int *z) {
-  int z_temp;
-  int i;
+  int z_temp, i, sign;
   
   clock_t start = clock();
 
-  int32x2_t xy_neon = {xy & 0xffff, (xy >> 16) & 0xffff};
-  int32x2_t yx_neon = vrev64_s32(xy_neon);
+  int32x2_t yx_neon = {(xy >> 16) & 0xffff, xy & 0xffff};
+  int32x2_t xy_neon = vrev64_s32(yx_neon);
 
   z_temp = 0;
 
-  int32x2_t sign;
-  int32x2_t sign_A = {1, -1};
-  int32x2_t sign_B = {-1, 1};
-
   for( i=0; i<15; i++) { /* 15 iterations are needed */
       
-    sign = xy_neon[1] > 0 ? sign_A : sign_B;
+    sign = xy_neon[1] > 0 ? 1 : -1;
 
     // shift
     yx_neon[0] = yx_neon[0] >> i;
     yx_neon[1] = yx_neon[1] >> i;
 
-    yx_neon = vmul_s32(yx_neon, sign); // fix signs
+    z_temp += sign*z_table[i]; // compute z
+
+    // fix signs
+    yx_neon[0] *=sign;
+    yx_neon[1] *=-sign;
 
     xy_neon = vadd_s32(xy_neon, yx_neon); // add
 
     yx_neon = vrev64_s32(xy_neon); // fix orientation
-
-    z_temp += sign[0]*z_table[i]; // compute z
 
   }
   
@@ -44,3 +41,4 @@ int cordic_V_fixed_point(int xy, int *z) {
 
   return xy_neon[1] << 16 | xy_neon[0];
 }
+
--- slides_code/cordic_V_fixed_point_neon.s
+++ slides_code/cordic_V_fixed_point_neon.s
@@ -21,45 +21,43 @@
 cordic_V_fixed_point:
 	@ args = 0, pretend = 0, frame = 8
 	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, lr}
+	push	{r4, r5, r6, lr}
 	vpush.64	{d8}
 	mov	r5, r0
-	sub	sp, sp, #12
+	sub	sp, sp, #8
 	mov	r4, r1
 	bl	clock
-	uxth	r3, r5
-	lsr	r5, r5, #16
+	lsr	r3, r5, #16
+	uxth	r5, r5
 	stm	sp, {r3, r5}
+	mov	r2, #0
+	vldr	d16, [sp]
 	mov	r6, r0
-	vldr	d8, [sp]
-	mov	r0, #0
-	vrev64.32	d16, d8
-	mov	r5, r0
+	vrev64.32	d8, d16
+	mov	r5, r2
 	ldr	lr, .L8
 .L3:
-	vmov.32	r2, d16[0]
+	vmov.32	r3, d16[0]
 	vmov.32	r1, d8[1]
-	asr	r2, r2, r0
-	vmov.32	r3, d16[1]
-	vmov.32	d16[0], r2
+	asr	r3, r3, r2
+	vmov.32	r0, d16[1]
+	vmov.32	d16[0], r3
 	cmp	r1, #0
-	movgt	ip, #1
-	mvnle	ip, #0
-	asr	r3, r3, r0
-	vmov.32	d16[1], r3
-	mvngt	r1, #0
-	movle	r1, #1
-	mul	r7, ip, r2
-	mul	r2, r2, r7
-	vmov.32	d16[0], r2
-	mul	r2, r1, r3
-	mul	r3, r3, r2
-	vmov.32	d16[1], r3
+	movgt	r1, #1
+	mvnle	r1, #0
+	asr	r0, r0, r2
+	vmov.32	d16[1], r0
+	mul	r3, r1, r3
+	mvngt	ip, #0
+	movle	ip, #1
+	vmov.32	d16[0], r3
+	mul	r0, ip, r0
+	vmov.32	d16[1], r0
 	vadd.i32	d8, d8, d16
+	add	r2, r2, #1
 	ldr	r3, [lr, #4]!
-	add	r0, r0, #1
-	cmp	r0, #15
-	mla	r5, r3, ip, r5
+	cmp	r2, #15
+	mla	r5, r3, r1, r5
 	vrev64.32	d16, d8
 	bne	.L3
 	bl	clock
@@ -71,10 +69,10 @@ cordic_V_fixed_point:
 	vmov.32	r3, d8[0]
 	orr	r0, r3, r0, lsl #16
 	str	r5, [r4]
-	add	sp, sp, #12
+	add	sp, sp, #8
 	@ sp needed
 	vldm	sp!, {d8}
-	pop	{r4, r5, r6, r7, pc}
+	pop	{r4, r5, r6, pc}
 .L9:
 	.align	2
 .L8:
